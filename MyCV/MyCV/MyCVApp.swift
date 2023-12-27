c8bafff44e29231e2a44b06369703a44088ad732
Gabriel Castro <gcalvarez93@gmail.com>
2023-12-27 12:10:30 +0100

    Initial Commit

---
+++ MyCV/MyCV/MyCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCVApp.swift
+//  MyCV
+//
+//  Created by Gabriel Castro on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MyCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
